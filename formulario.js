35d321a4eb00b3c48c18d1a89875048cb139b97a
Fernando Panduro <fernandogp2003pro@gmail.com>
2021-11-08 21:04:09 -0600

    Primera fase del desarrollo de la app, Terminada

---
+++ formulario.js
@@ -0,0 +1,2 @@
+//Escribir una página en HTML y JavaScript, con un formulario que el usuario llenará y realizará distintas validaciones. Por ejemplo, que los campos obligatorios sean llenados, que los campos que deben contener números no contengan letras, etc. 
+
